a9ef7dcaa0c940d01ef3b698a1271c64e35755e2
Ilya <79043022759@yandex.ru>
2025-08-11 22:03:02 +0300

    keywords

--- ui/src/components/ContactsSection.jsx
+++ ui/src/components/ContactsSection.jsx
@@ -10,14 +10,14 @@ const ContactsSection = () => {
             <h2 className={styles.titleSection}>Ждём вас <br/> в наших магазинах</h2>
             <div className={styles.contactsItems}>
               <div className={styles.contactsItem}>
-                <h3 className={styles.contactsSubtitle}>Москва, ул. Ленина 1</h3>
+                <h3 className={styles.contactsSubtitle}>Москва, ул. Петровка 7</h3>
                 <p className={styles.contactsText}>
                   Каждый день с 8:00 до 20:00 <br />
                   Без перерывов и выходных
                 </p>
               </div>
               <div className={styles.contactsItem}>
-                <h3 className={styles.contactsSubtitle}>Париж, ул. Сталина 2</h3>
+                <h3 className={styles.contactsSubtitle}>Краснодар, ул. Красная 12</h3>
                 <p className={styles.contactsText}>
                   Каждый день с 8:00 до 20:00 <br />
                   Без перерывов и выходных
